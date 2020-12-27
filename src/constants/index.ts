e1c9a9dbb26a40d53e5223878576d4338a9531ee
Tony <jeehyukwon@gmail.com>
2020-12-27 22:01:24 +0900

    refactor: add comments in constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,9 @@
+/**
+ * The current stage of the server is running on
+ */
 export const STAGE = process.env.STAGE as 'development' | 'alpha' | 'production'
+
+/**
+ * MongoDB endpoint
+ */
 export const MONGO_ENDPOINT = process.env.MONGO_ENDPOINT as string
